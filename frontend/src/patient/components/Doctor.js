f6bea87e186e8720996c1741cd605dd3799a973c
Yash H Patel <yashhpatel.ict18@gmail.com>
2021-05-29 15:26:49 +0530

    Doctor \.js updated

--- frontend/src/patient/components/Doctor.js
+++ frontend/src/patient/components/Doctor.js
@@ -1,19 +1,28 @@
 import React from "react";
+import DrM from '../../Photos/Dr_M.png';
+import DrF from '../../Photos/Dr_F.png';
 
 const Doctor = (props) => {
     return(
         <React.Fragment>
-            <div>
-                <h5>Name: {props.name}</h5>
-                <p>Email: {props.email}</p>
-                <p>Contact No.: {props.phoneNo}</p>
-                <p>City: {props.city}</p>
-                <p>State: {props.state}</p>
-                {props.gender==="Male" ? "M" : "F"}
-                <p>Designation: {props.designation}</p>
-                <p>Current Patients: {props.currentPatientsLength}</p>
-                <button>Consult</button>
-                <br />
+            <div m2 text-center>                
+                <div style={{float:"left",marginLeft:300}}>
+                    {props.gender==="Male" ? <img style={{width: 50,height: 50,borderRadius:80}} src={DrM} alt="Male Dr." /> : <img style={{width: 50,height: 50,borderRadius:80}} src={DrF} alt="Female Dr." /> }
+                    <h5>Name: {props.name}</h5>                
+                </div>
+                <div style={{marginLeft:300}}>
+                    <div style={{float:"left"}}>
+                        <p>Designation: {props.designation}</p>
+                        <p>Email: {props.email}</p>
+                        <p>Contact No.: {props.phoneNo}</p>
+                        <p>Current Patients: {props.currentPatientsLength}</p>
+                    </div>
+                    <div style={{marginLeft:20,float:"left"}}>
+                        <p>City: {props.city}</p>
+                        <p>State: {props.state}</p>                           
+                        <button>Consult</button>         
+                    </div>                                  
+                </div>
             </div>
         </React.Fragment>
     );
