8437a835ad39d1a7261a679f5a30e7587ea79f13
ishan25j <ishan25j@gmail.com>
2021-05-30 09:54:08 +0530

    medicine backend fix

--- Backend/models/medicine-model.js
+++ Backend/models/medicine-model.js
@@ -2,27 +2,25 @@ const mongoose = require("mongoose");
 
 const medicineSchema = new mongoose.Schema({
 
-    name:{ type:String , required:true },
+    name: { type: String, required: true },
 
-    duration:{type:Number , required:true},
+    duration: { type: Number, required: true },
 
-    active:{type:Boolean , required:true},
+    active: { type: Boolean, required: true },
 
-    quantity:{ type:Number , required:true },
+    patientId: { type: mongoose.Types.ObjectId, required: true, ref: 'Patient' },
 
-    patientId:{type:mongoose.Types.ObjectId , required:true , ref:'Patient'},
+    doctorId: { type: mongoose.Types.ObjectId, required: true, ref: 'Doctor' },
 
-    doctorId:{type:mongoose.Types.ObjectId , required:true , ref:'Doctor'},
-
-    time:{
-        morningBeforeB:{type:Number},
-        morningAfterB:{type:Number },
-        afternoonBeforeL:{type:Number },
-        afternoonAfterL:{type:Number },
-        eveningBeforeD:{type:Number },
-        eveningAfterD:{type:Number }
+    time: {
+        morningBeforeB: { type: Number },
+        morningAfterB: { type: Number },
+        afternoonBeforeL: { type: Number },
+        afternoonAfterL: { type: Number },
+        eveningBeforeD: { type: Number },
+        eveningAfterD: { type: Number }
     }
 });
 
 
-module.exports = mongoose.model('Medicine',medicineSchema);
+module.exports = mongoose.model('Medicine', medicineSchema);
